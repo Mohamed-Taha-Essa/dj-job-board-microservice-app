9bbb485b2c278bd457cd4e28825da1b1ff1602e0
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-09-09 15:00:11 +0300

    add custom pagination

--- jobs/job/views.py
+++ jobs/job/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from rest_framework import filters
 
+
 from .models import Job , JobApply
 from .serializers import JobApplySerializer  , JobSerializer
-
+from .pagination import JobPagination
 
 class JobsListCreateAPI(generics.ListCreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+
+    pagination_class =JobPagination
 
 
     
