a213a7235e667d46f6a713369171130571791d01
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-09-10 12:37:04 +0300

    add swagger docs

--- jobs/project/urls.py
+++ jobs/project/urls.py
@@ -16,11 +16,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
+
+
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from django.conf.urls.static import static
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Jobs API",
+      default_version='v1',
+      description="Job Board API",
+      contact=openapi.Contact(email="pythondevloper33@gmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("jobs/", include('job.urls'))
+    path("jobs/", include('job.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
