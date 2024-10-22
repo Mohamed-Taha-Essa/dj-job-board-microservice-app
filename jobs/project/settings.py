dde9f3df5c12ea0521a9c63e21353365edf7a874
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-10-22 09:51:28 +0300

    add corse-header

--- jobs/project/settings.py
+++ jobs/project/settings.py
@@ -53,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
 
 ROOT_URLCONF = 'project.urls'
 
