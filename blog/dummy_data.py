109dad7e0b9be68b3dd1f02ef4b7740e2ac2febd
Mohamed-Taha-Essa <www.taha.166@gmail.com>
2024-10-22 09:00:26 +0300

    add dummy data to blog

---
+++ blog/dummy_data.py
@@ -0,0 +1,45 @@
+import os , django 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+django.setup()
+
+import random
+from faker import Faker
+from post.models import  Post , PostLikes , Comment
+
+
+def generate_fake_posts(num=10):
+    fake = Faker()
+    for _ in range(num):
+        title = fake.job()
+        Post.objects.create(
+            title = title,
+            content = fake.paragraph(nb_sentences=10),
+            publish_date = fake.date() , 
+            author_id = random.randint(1,20)
+        )
+        
+
+def generate_fake_post_likes(num=10):
+    posts = Post.objects.all()
+    for _ in range(num):
+        PostLikes.objects.create(
+            user_id = random.randint(1,100),
+            post = random.choice(posts)
+        )
+            
+
+def generate_fake_comments(num=10):
+    posts = Post.objects.all()
+    fake = Faker()
+    for _ in range(num):
+        Comment.objects.create(
+            user_id = random.randint(1,100),
+            post = random.choice(posts),
+            content = fake.text(),
+            comment_date = fake.date()
+        )
+
+
+generate_fake_posts(100)
+generate_fake_post_likes(200)
+generate_fake_comments(200)
