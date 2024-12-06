3ae6d9cf317eac28464c27d05b50da355e9ca287
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-12-06 19:42:08 +0200

    add form in ui to add job

--- frontend/app/jobs/page.tsx
+++ frontend/app/jobs/page.tsx
@@ -1,11 +1,19 @@
 import JobsFilter from "@/components/jobs/JobsFilter"
 import JobsList from "@/components/jobs/JobsList"
+import { Button } from "@/components/ui/button"
 
-
+import { Link } from "lucide-react"
 
 function JobListPage(){
     return(
         <div className="container ">
+            <div className="flex justif-between ">
+            <h1 className="text-3xl font-bold mb-6 text-center">Latest Posts</h1>
+            <Link href="/jobs/add-job">
+                <Button >Add New Job </Button>
+            </Link>
+            
+            </div>
            <div className="flex">
                 <JobsFilter/>
                 
